1fe5213b8a5466ee91020b1d64e64730adf7b52b
Nicholas Thomson <nicholasgraythomson@gmail.com>
2021-04-12 09:08:15 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your code here!
+//1
+document.querySelector('main').remove();
+
+
+//2
+
+let newHeader = document.createElement('h1');
+
+//3
+
+
+newHeader.id = 'victory';
+
+document.getElementById('newHeader');
+newHeader.innerHTML = "Nicholas is the champion.";
+
+
